d8d037dae74e0675758983117c3d68749de7b819
treynoso <thomasreynoso602@gmail.com>
2025-07-06 17:19:26 -0300

    Update AttendancePage with attendance management features and styling improvements

--- src/pages/AttendancePage/AttendancePage.styled.tsx
+++ src/pages/AttendancePage/AttendancePage.styled.tsx
@@ -3,12 +3,62 @@ export const Styles = {
     pageTitle: 'flex flex-col gap-2',
     title: 'text-4xl font-bold',
     description: 'text-gray-600 dark:text-gray-400',
-    button: 'bg-gradient-to-r from-primary to-secondary dark:from-dark-primary dark:to-dark-secondary text-white px-4 py-2 rounded-md flex flex-row gap-2 items-center',
+    button: `px-4 py-2 rounded-md flex flex-row gap-2 items-center text-white transition-all duration-200 ease-in-out
+        hover:bg-gradient-to-r hover:from-primary/90 hover:to-secondary/90
+        dark:hover:from-dark-primary/90 dark:hover:to-dark-secondary/90
+        bg-gradient-to-r from-primary to-secondary 
+        dark:from-dark-primary dark:to-dark-secondary`,
     filtersContainer:
         'mt-8 px-4 py-4 xl:px-4 border border-gray-300 dark:border-zinc-700 rounded-lg',
-    filtersTitle: 'flex flex-row gap-2 items-center text-xl font-bold',
+    filtersTitle: `flex flex-row gap-2 items-center text-xl font-bold
+        text-gray-900 dark:text-gray-400`,
     filtersContent: 'mt-4 py-2 flex flex-col xl:flex-row xl:justify-between gap-2',
-    filtersContentItem: 'flex flex-col gap-2 xl:w-1/3',
-    filtersContentItemLabel: 'text-gray-600 dark:text-gray-400',
-    filtersContentItemInput: 'w-full p-1 border border-gray-300 dark:border-zinc-700 rounded-md',
+    filtersContentItem: `flex flex-col gap-2 xl:w-1/3`,
+    filtersContentItemLabel: 'text-gray-900 dark:text-gray-400',
+    filtersContentItemInput: `w-full p-1 border rounded-md
+        border-gray-300 dark:border-zinc-700
+        text-gray-900 dark:text-gray-400`,
+    dashboard: `
+        grid grid-cols-2 xl:grid-cols-5 gap-4
+        mt-8`,
+    dashboardItem: `flex flex-col gap-2 w-full
+        p-4
+        border border-gray-300
+        dark:border-zinc-700
+        rounded-lg`,
+    dashboardItemTitle: `flex flex-row gap-2 items-center text-xl font-bold
+        text-gray-900 dark:text-gray-400`,
+    dashboardItemTitleContent: 'flex flex-col',
+    dashboardItemTitleContentTitle: 'text-sm font-light text-gray-700 dark:text-gray-400',
+    dashboardItemTitleContentValue: 'text-3xl font-bold',
+    studentsAttendance: 'mt-8 p-4 border border-gray-300 dark:border-zinc-700 rounded-lg',
+    studentsAttendanceHeader: 'flex flex-col gap-2',
+    studentsAttendanceHeaderTitle: 'text-2xl font-bold text-gray-900 dark:text-gray-400',
+    studentsAttendanceHeaderDescription: 'text-gray-600 dark:text-gray-400',
+    studentsAttendanceList: 'mt-4 flex flex-col gap-4',
+    studentsAttendanceListItem:
+        'flex flex-row gap-2 px-4 py-3 justify-between border border-gray-300 dark:border-zinc-700 rounded-lg text-gray-900 dark:text-gray-400 items-center',
+    studentsAttendanceListItemName: 'text-lg font-bold',
+    studentsAttendanceListItemActions: 'flex flex-row gap-2',
+    studentsAttendanceListItemAction: `px-2 py-1 rounded-md border border-gray-300 dark:border-zinc-700
+        text-gray-900 dark:text-gray-400`,
+    studentsAttendanceFooter: 'flex flex-row gap-2 justify-end mt-4',
+    studentsAttendanceFooterCancelButton: `
+        px-4 py-2 rounded-md 
+        font-semibold
+        border border-gray-300 dark:border-zinc-700
+        text-gray-900 dark:text-gray-400
+        transition-all duration-200 ease-in-out
+        hover:bg-gray-200 dark:hover:bg-zinc-700
+        `,
+    studentsAttendanceFooterSaveButton: `
+        px-4 py-2 rounded-md border 
+        border-gray-300 dark:border-zinc-700
+        bg-gradient-to-r from-primary to-secondary
+        dark:from-dark-primary dark:to-dark-secondary
+        text-white font-semibold
+        transition-all duration-200 ease-in-out
+        hover:bg-gradient-to-r hover:from-primary/90 hover:to-secondary/90
+        dark:hover:from-dark-primary/90 dark:hover:to-dark-secondary/90
+        `,
 };
--- src/pages/AttendancePage/AttendancePage.tsx
+++ src/pages/AttendancePage/AttendancePage.tsx
@@ -1,10 +1,61 @@
+import { useState } from 'react';
 import type { Props as ContainerProps } from './AttendancePage.container';
 import { Styles } from './AttendancePage.styled';
 
 interface Props extends ContainerProps {}
 
+const students = [
+    {
+        id: 1,
+        name: 'Juan Perez',
+        attendance: 'present',
+    },
+    {
+        id: 2,
+        name: 'Maria Gomez',
+        attendance: 'late',
+    },
+    {
+        id: 3,
+        name: 'Pedro Gomez',
+        attendance: 'absent',
+    },
+    {
+        id: 4,
+        name: 'Ana Gomez',
+        attendance: 'present',
+    },
+];
+
+const attendanceStatus = [
+    {
+        color: 'bg-green-300 text-green-800 dark:bg-green-800 dark:text-green-300',
+        label: 'Presente',
+        code: 'present',
+    },
+    {
+        color: 'bg-yellow-300 text-yellow-800 dark:bg-yellow-800 dark:text-yellow-300',
+        label: 'Tarde',
+        code: 'late',
+    },
+    {
+        color: 'bg-red-300 text-red-800 dark:bg-red-800 dark:text-red-300',
+        label: 'Ausente',
+        code: 'absent',
+    },
+];
+
 export const AttendancePage = (props: Props) => {
+    const [studentsAttendance, setStudentsAttendance] = useState(students);
     const {} = props;
+    const handleAttendance = (id: number, attendance: string) => {
+        setStudentsAttendance(prev =>
+            prev.map(student => (student.id === id ? { ...student, attendance } : student)),
+        );
+    };
+    const handleAttendanceAll = (attendance: string) => {
+        setStudentsAttendance(prev => prev.map(student => ({ ...student, attendance })));
+    };
     return (
         <section>
             <div className={Styles.pageHeader}>
@@ -14,7 +65,7 @@ export const AttendancePage = (props: Props) => {
                         Acá podés gestionar el presentismo de los alumnos.
                     </p>
                 </div>
-                <button className={Styles.button}>
+                <button className={Styles.button} onClick={() => handleAttendanceAll('present')}>
                     <i className='fa fa-check-square' aria-hidden='true'></i>
                     <span>Marcar todos presentes</span>
                 </button>
@@ -63,6 +114,120 @@ export const AttendancePage = (props: Props) => {
                         />
                     </div>
                 </div>
+            </div>
+            <div className={Styles.dashboard}>
+                <div className={Styles.dashboardItem}>
+                    <div className={Styles.dashboardItemTitle}>
+                        <i className='fa fa-users text-blue-500' aria-hidden='true'></i>
+                        <div className={Styles.dashboardItemTitleContent}>
+                            <span className={Styles.dashboardItemTitleContentTitle}>Total</span>
+                            <span
+                                className={
+                                    Styles.dashboardItemTitleContentValue +
+                                    ' text-gray-900 dark:text-gray-400'
+                                }>
+                                8
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div className={Styles.dashboardItem}>
+                    <div className={Styles.dashboardItemTitle}>
+                        <i
+                            className='fa-regular fa-circle-check text-green-500'
+                            aria-hidden='true'></i>
+                        <div className={Styles.dashboardItemTitleContent}>
+                            <span className={Styles.dashboardItemTitleContentTitle}>Presentes</span>
+                            <span
+                                className={
+                                    Styles.dashboardItemTitleContentValue + ' text-green-500'
+                                }>
+                                0
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div className={Styles.dashboardItem}>
+                    <div className={Styles.dashboardItemTitle}>
+                        <i className='fa-regular fa-clock text-yellow-500' aria-hidden='true'></i>
+                        <div className={Styles.dashboardItemTitleContent}>
+                            <span className={Styles.dashboardItemTitleContentTitle}>Tarde</span>
+                            <span
+                                className={
+                                    Styles.dashboardItemTitleContentValue + ' text-yellow-500'
+                                }>
+                                0
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div className={Styles.dashboardItem}>
+                    <div className={Styles.dashboardItemTitle}>
+                        <i className='fa-solid fa-user-slash text-red-500' aria-hidden='true'></i>
+                        <div className={Styles.dashboardItemTitleContent}>
+                            <span className={Styles.dashboardItemTitleContentTitle}>Ausentes</span>
+                            <span
+                                className={Styles.dashboardItemTitleContentValue + ' text-red-500'}>
+                                0
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div className={Styles.dashboardItem}>
+                    <div className={Styles.dashboardItemTitle}>
+                        <i className='fa fa-users text-gray-500' aria-hidden='true'></i>
+                        <div className={Styles.dashboardItemTitleContent}>
+                            <span className={Styles.dashboardItemTitleContentTitle}>
+                                Sin Marcar
+                            </span>
+                            <span
+                                className={
+                                    Styles.dashboardItemTitleContentValue + ' text-gray-500'
+                                }>
+                                8
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={Styles.studentsAttendance}>
+                <div className={Styles.studentsAttendanceHeader}>
+                    <h2 className={Styles.studentsAttendanceHeaderTitle}>
+                        Asistencia de Alumnos - 1A
+                    </h2>
+                    <p className={Styles.studentsAttendanceHeaderDescription}>
+                        Registrá la asistencia de 8 alumnos el 6/7/2025
+                    </p>
+                </div>
+                <div className={Styles.studentsAttendanceList}>
+                    {studentsAttendance.map((student, index) => (
+                        <div className={Styles.studentsAttendanceListItem} key={index}>
+                            <p className={Styles.studentsAttendanceListItemName}>{student.name}</p>
+                            <div className={Styles.studentsAttendanceListItemActions}>
+                                {attendanceStatus.map(status => (
+                                    <button
+                                        onClick={() => handleAttendance(student.id, status.code)}
+                                        key={status.label}
+                                        className={
+                                            Styles.studentsAttendanceListItemAction +
+                                            ' ' +
+                                            (student.attendance === status.code
+                                                ? status.color
+                                                : 'text-gray-800 dark:bg-gray-700 dark:text-gray-400')
+                                        }>
+                                        {status.label}
+                                    </button>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className={Styles.studentsAttendanceFooter}>
+                <button className={Styles.studentsAttendanceFooterCancelButton}>Cancelar</button>
+                <button className={Styles.studentsAttendanceFooterSaveButton}>
+                    Guardar Asistencias
+                </button>
             </div>
         </section>
     );
